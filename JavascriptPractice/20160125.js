8bff7c100eafc46e5b69c8467e44b5a3744e30b9
Zhao Kang <1003994970@qq.com>
2016-01-25 21:53:18 +0800

    regular

---
+++ JavascriptPractice/20160125.js
@@ -0,0 +1,24 @@
+// 1.
+// 因为rest参数是ES6新标准，所以你需要测试一下浏览器是否支持。
+// 请用rest参数编写一个sum()函数，接收任意个参数并返回它们的和：
+'use strict';
+function sum(...rest) {
+    let s = 0;
+    for (let i of rest) {
+        s += i;        
+    }
+    return s;
+}
+
+// 2.
+// 定义一个计算圆面积的函数area_of_circle()，它有两个参数：
+// r: 表示圆的半径；
+// pi: 表示π的值，如果不传，则默认3.14
+'use strict';
+
+function area_of_circle(r, pi) {
+	if (arguments.length === 1)
+		pi = 3.14;
+    let area = pi * r * r;
+    return area;
+}
