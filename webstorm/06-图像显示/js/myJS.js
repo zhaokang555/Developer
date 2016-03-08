91f018dd78599bc2197968c5f4727fe0b9306b30
Zhao Kang <1003994970@qq.com>
2016-03-08 18:51:02 +0800

    remove isCanvasOnFocus

--- webstorm/06-图像显示/js/myJS.js
+++ webstorm/06-图像显示/js/myJS.js
@@ -6,7 +6,7 @@
     var canvas = document.getElementById('myCanvas');
     var context = canvas.getContext('2d');
     var imgObj = new Image();
-    var isCanvasOnFocus = false;
+    // var isCanvasOnFocus = false;
     var imgWidth = 600;
     var imgHeight = 600;
     var imgWidthScale = imgWidth / 600;
@@ -18,13 +18,15 @@
 
     imgObj.src = './img/map.png';
 
-    canvas.addEventListener('onmouseover', function () {
-        isCanvasOnFocus = true;
-    }, false);
+    // canvas.addEventListener('mouseover', function () {
+    //     isCanvasOnFocus = true;
+    //     console.log('isCanvasOnFocus = true');
+    // }, false);
 
-    canvas.addEventListener('onmouseout', function () {
-        isCanvasOnFocus = false;
-    }, false);
+    // canvas.addEventListener('mouseout', function () {
+    //     isCanvasOnFocus = false;
+    //     console.log('isCanvasOnFocus = false');
+    // }, false);
 
     canvas.addEventListener('mousewheel', function (event) {
         //cross-browser wheel delta
@@ -32,12 +34,13 @@
         var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
 
         //console.log('delta: ' +  delta);
-
+        // if (isCanvasOnFocus) {
         imgWidth = Math.max(600, imgWidth + delta*imgWidth/30);
         imgHeight = Math.max(600, imgHeight + delta*imgHeight/30);
 
-
         context.drawImage(imgObj, 0, 0, imgWidth, imgHeight);
+        // }
+        
     }, false);
 
 })();
