a2d04de0dc7fcac36d9a6ba6ecc11857a2302deb
Zhao Kang <1003994970@qq.com>
2016-03-28 15:15:24 +0800

    learn node

---
+++ node/15-child_process-test/support.js
@@ -0,0 +1,6 @@
+// console.log("进程 " + process.argv[2] + " 执行。" );
+console.log("进程: \n" 
+            + process.argv[0] + "\n" 
+            + process.argv[1] + "\n" 
+            + process.argv[2] + "\n" 
+            + " 执行。");
